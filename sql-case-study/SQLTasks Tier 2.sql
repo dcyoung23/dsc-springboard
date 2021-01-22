772965df93a02a0c24f96fe954d6391b031d2fb0
D. Chris Young <dcyoung23@gmail.com>
2021-01-21 20:38:58 -0800

    Update sql formatting

--- sql-case-study/SQLTasks Tier 2.sql
+++ sql-case-study/SQLTasks Tier 2.sql
@@ -35,73 +35,85 @@ exploring the data, and getting acquainted with the 3 tables. */
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Write a SQL query to produce a list of the names of the facilities that do. */
 SELECT 
-name
-FROM Facilities
-WHERE membercost > 0
+  name
+FROM 
+  Facilities
+WHERE 
+  membercost > 0
 
 /* Q2: How many facilities do not charge a fee to members? */
 -- This query makes it eaier to add in other facility conditional aggregates as well
 -- Answer = 4
 SELECT 
-SUM(CASE WHEN membercost = 0 THEN 1 ELSE 0 END) AS no_member_cost
-FROM Facilities
+  SUM(CASE WHEN membercost = 0 THEN 1 ELSE 0 END) AS no_member_cost
+FROM 
+  Facilities
 
 /* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
 where the fee is less than 20% of the facility's monthly maintenance cost.
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
 SELECT
-facid
-,name
-,membercost
-,monthlymaintenance
-FROM Facilities
-WHERE membercost > 0
-AND membercost < (monthlymaintenance * .2)
+  facid
+  ,name
+  ,membercost
+  ,monthlymaintenance
+FROM 
+  Facilities
+WHERE 
+  membercost > 0
+  AND membercost < (monthlymaintenance * .2)
 
 /* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
 Try writing the query without using the OR operator. */
 SELECT
-*
-FROM Facilities
-WHERE facid in (1,5)
+  *
+FROM 
+  Facilities
+WHERE
+  facid in (1,5)
 
 /* Q5: Produce a list of facilities, with each labelled as
 'cheap' or 'expensive', depending on if their monthly maintenance cost is
 more than $100. Return the name and monthly maintenance of the facilities
 in question. */
 SELECT 
-name
-,monthlymaintenance
-,CASE WHEN monthlymaintenance > 100 THEN 'expensive' else 'cheap' END AS monthlymaintenance_category
-FROM Facilities
+  name
+  ,monthlymaintenance
+  ,CASE WHEN monthlymaintenance > 100 THEN 'expensive' else 'cheap' END AS monthlymaintenance_category
+FROM
+  Facilities
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Try not to use the LIMIT clause for your solution. */
 -- Normally would have used a Window Function here but the MySQL version is < 8
 SELECT
-firstname
-,surname
-FROM Members
-WHERE joindate in (SELECT MAX(joindate) FROM Members)
+  firstname
+  ,surname
+FROM
+  Members
+WHERE
+  joindate in (SELECT MAX(joindate) FROM Members)
 
 /* Q7: Produce a list of all members who have used a tennis court.
 Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
 SELECT
-F.name
-,CONCAT(M.firstname,' ',M.surname) as member_name
-,SUM(F.membercost) AS membercost_total
-FROM Members M
-INNER JOIN Bookings B ON M.memid = B.memid
-INNER JOIN Facilities F ON B.facid = F.facid
-WHERE F.name like '%Tennis Court%'
+  F.name
+  ,CONCAT(M.firstname,' ',M.surname) as member_name
+  ,SUM(F.membercost) AS membercost_total
+FROM 
+  Members M
+  INNER JOIN Bookings B ON M.memid = B.memid
+  INNER JOIN Facilities F ON B.facid = F.facid
+WHERE 
+  F.name like '%Tennis Court%'
 GROUP BY
-F.name
-,member_name
+  F.name
+  ,member_name
 ORDER BY
-member_name
+  member_name
 
 /* Q8: Produce a list of bookings on the day of 2012-09-14 which
 will cost the member (or guest) more than $30. Remember that guests have
@@ -110,42 +122,48 @@ the guest user's ID is always 0. Include in your output the name of the
 facility, the name of the member formatted as a single column, and the cost.
 Order by descending cost, and do not use any subqueries. */
 SELECT 
-F.name
-,CASE WHEN M.memid = 0 THEN 'Guest' ELSE CONCAT( M.firstname, ' ', M.surname ) END AS member_name
-,SUM(CASE WHEN M.memid = 0 THEN F.guestcost ELSE F.membercost END * B.slots ) AS cost
-FROM Bookings B
-INNER JOIN Facilities F ON B.facid = F.facid
-LEFT JOIN Members M ON B.memid = M.memid
-WHERE DATE(starttime) = '2012-09-14'
+  F.name
+  ,CASE WHEN M.memid = 0 THEN 'Guest' ELSE CONCAT( M.firstname, ' ', M.surname ) END AS member_name
+  ,SUM(CASE WHEN M.memid = 0 THEN F.guestcost ELSE F.membercost END * B.slots ) AS cost
+FROM 
+  Bookings B
+  INNER JOIN Facilities F ON B.facid = F.facid
+  LEFT JOIN Members M ON B.memid = M.memid
+WHERE 
+  DATE(starttime) = '2012-09-14'
 GROUP BY 
-F.name
-,member_name
-HAVING(cost) > 30
+  F.name
+  ,member_name
+HAVING 
+  (cost) > 30
 ORDER BY 
-cost DESC
+  cost DESC
 
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
 SELECT
 name
-,member_name
-,SUM(cost) AS cost
+  ,member_name
+  ,SUM(cost) AS cost
 FROM 
 (
 SELECT 
-F.name
-,CASE WHEN M.memid = 0 THEN 'Guest' ELSE CONCAT( M.firstname, ' ', M.surname ) END AS member_name
-,CASE WHEN M.memid = 0 THEN F.guestcost ELSE F.membercost END * B.slots AS cost
-FROM Bookings B
-INNER JOIN Facilities F ON B.facid = F.facid
-LEFT JOIN Members M ON B.memid = M.memid
-WHERE DATE(starttime) = '2012-09-14'
+  F.name
+  ,CASE WHEN M.memid = 0 THEN 'Guest' ELSE CONCAT( M.firstname, ' ', M.surname ) END AS member_name
+  ,CASE WHEN M.memid = 0 THEN F.guestcost ELSE F.membercost END * B.slots AS cost
+FROM 
+  Bookings B
+  INNER JOIN Facilities F ON B.facid = F.facid
+  LEFT JOIN Members M ON B.memid = M.memid
+WHERE 
+  DATE(starttime) = '2012-09-14'
 ) X
 GROUP BY
-name
-,member_name
-HAVING cost > 30
+  name
+  ,member_name
+HAVING 
+  cost > 30
 ORDER BY
-cost DESC
+  cost DESC
 
 /* PART 2: SQLite
 
